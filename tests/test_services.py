f68ee9a10e43e2247d9f3432204c7539a295347c
Zhanming <zcui@akamai.com>
2024-12-05 14:51:31 +0000

    Init commit

---
+++ tests/test_services.py
@@ -0,0 +1,30 @@
+from python_project1.services import declaim
+
+class TestDeclaim:
+    def test_declaim(self, capsys):
+        title = "Super Cool"
+        poem = (
+            "Roses are red\n"
+            "Violets are blue\n"
+            "I think the Global South\n"
+            "is super cool"
+        )
+        author = "Tonino"
+        expected_output = (
+            "---------------------------------------------------\n"
+            "Super Cool\n"
+            "\n"
+            "Roses are red\n"
+            "Violets are blue\n"
+            "I think the Global South\n"
+            "is super cool\n"
+            "\n"
+            "By Tonino\n"
+            "---------------------------------------------------\n"
+        )
+
+        declaim(title, poem, author)
+
+        captured = capsys.readouterr()
+
+        assert captured.out == expected_output
